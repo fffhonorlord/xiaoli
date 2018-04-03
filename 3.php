3001b7b950599331011f264e40be99dcc224e852
fffhonorlord <feiyingruyu@163.com>
2018-04-03 10:17:34 +0800

    change all

--- 3.php
+++ 3.php
@@ -1,1 +1,26 @@
-啊实打实的
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge  chrome=1">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "2333";
+?>    
+
+<script>    
+
+
+
+
+</script>
+
+
+
+
+
+</body>
+</html>
