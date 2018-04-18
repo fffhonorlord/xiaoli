07c91ea894effbfcfbf4908559a373cbd8f8a8ff
fffhonorlord <feiyingruyu@163.com>
2018-04-18 17:32:19 +0800

    commit change /4.18

--- 1.php
+++ 1.php
@@ -7,8 +7,50 @@
     <title>Document</title>
 </head>
 <body>
-    233
+    <!-- 233 -->
+<?php
+define("d1",1);
+define("d2",2);
+define("d3",4);
 
+$state=5;
+
+// if  (($state&d1)>0)
+// {
+// echo ("<br /> 1dengliang");
+// }
+// if  (($state&d2)>0)
+// {
+// echo ("<br /> 2dengliang");
+// }
+
+
+function showall($sta)
+{
+for ($i=1;$i<4;$i++)
+{
+$s="d".$i;
+if (($sta&constant($s))>0)
+{
+echo "deng {$i} liang";
+}
+}
+}
+showall(5);
+echo "<hr> ";
+$state=$state|d2;
+showall($state);
+echo "<hr> ";
+$state=$state&(~d1);
+showall($state);
+echo "<hr> ";
+?>
+<?php 
+include '.\3.php';
+ $v2=  include __DIR__ . '\3.php';
+echo "($v2)";
+
+?>
 
 
 
--- 3.php
+++ 3.php
@@ -8,7 +8,9 @@
 </head>
 <body>
 <?php
+$v1=123;
 echo "2333";
+return $v1;
 ?>    
 
 <script>    
